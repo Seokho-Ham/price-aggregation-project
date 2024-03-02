72c0a595ec7d4f5a2ea974533aca5db2515e43ed
SeokHo-Ham <forkyy.dev@gmail.com>
2024-03-02 15:25:44 +0900

    feat: BaseTimeEntity 추가

---
+++ backend/src/main/java/com/musinsa/assignment/common/config/ApplicationConfig.java
@@ -0,0 +1,9 @@
+package com.musinsa.assignment.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class ApplicationConfig {
+}
