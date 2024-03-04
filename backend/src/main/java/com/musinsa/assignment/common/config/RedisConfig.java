292806da8446415fc5f945c30d153cf2f9178dcc
SeokHo-Ham <forkyy.dev@gmail.com>
2024-03-04 16:33:19 +0900

    feat: redis 의존성 및 Config 클래스 추가

---
+++ backend/src/main/java/com/musinsa/assignment/common/config/RedisConfig.java
@@ -0,0 +1,22 @@
+package com.musinsa.assignment.common.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+@RequiredArgsConstructor
+@EnableRedisRepositories
+@Configuration
+public class RedisConfig {
+
+    private final RedisProperties redisProperties;
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(redisProperties.getHost(), redisProperties.getPort());
+    }
+
+}
